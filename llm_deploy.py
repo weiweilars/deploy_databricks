d0b3244e169959e19ebb820c86e595febdb7aad3
WeiWei Feng <weiwei_feng8191@hotmail.com>
2023-11-09 11:43:24 +0100

    add grpcio requirements

--- llm_deploy.py
+++ llm_deploy.py
@@ -1,30 +1,55 @@
 import os
 import yaml
-import logging
+import pandas as pd
+
 
 from huggingface_hub import snapshot_download
+from huggingface_hub._login import _login
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 import mlflow
 from mlflow.models import infer_signature
 from mlflow.models.signature import infer_signature
+from mlflow.tracking import MlflowClient
 
-from huggingface_hub._login import _login
+from pyspark.dbutils import DBUtils
+import SparkSession
 
+spark = SparkSession.builder.getOrCreate()
+dbutils = DBUtils(spark)
 
-def load_model(config_path):
-
-    with open(config_path, "r") as yaml_file:
-        config_data = yaml.safe_load(yaml_file)
+def load_model(config_data):
 
     _login(token=config_data['Secrete']['huggingface_hub'], add_to_git_credential=False)
-    model_path = os.path.join(config_data['Deployment']['model_download_path'],config_data['Name'])
+    model_path = os.path.join(config_data['Deployment']['model_download_path'],config_data['Model']['name'])
     snapshot_location = os.path.expanduser(model_path)
-    if ~model_path.isdir():
-        print('downloading the model file')
-        os.makedirs(snapshot_location, exist_ok=True)
-        snapshot_location = snapshot_download(repo_id="HuggingFaceH4/zephyr-7b-alpha", local_dir=snapshot_location)
+    os.makedirs(snapshot_location, exist_ok=True)
+    snapshot_location = snapshot_download(repo_id="HuggingFaceH4/zephyr-7b-alpha", local_dir=snapshot_location)
     
+    return snapshot_location
 
+def choose_workload_size(quantization, parameter_size):
+    workload_type = "GPU_MEDIUM"
+    if quantization =="8_bit":
+        if parameter_size < 18:
+            workload_type = "GPU_MEDIUM" 
+        elif parameter_size > 18 and parameter_size < 71:
+            workload_type = "GPU_MEDIUM_4"
+        elif parameter_size > 160:
+            print("Model can't be fit on available workload sizes")
+        else:
+            workload_type = "GPU_MEDIUM_8"
+    else:
+        if parameter_size < 11:
+            workload_type = "GPU_MEDIUM" 
+        elif parameter_size > 10 and parameter_size < 35:
+            workload_type = "GPU_MEDIUM_4"
+        elif parameter_size > 80:
+            print("Model can't be fit on available workload sizes, try 8_bit quantization")
+        else:
+            workload_type = "GPU_MEDIUM_8"
+    return workload_type
     
 
 class Deploy_model(mlflow.pyfunc.PythonModel):
@@ -33,22 +58,11 @@ class Deploy_model(mlflow.pyfunc.PythonModel):
         This method initializes the tokenizer and language model
         using the specified model snapshot directory.
         """
-        from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
-
 
         # Initialize tokenizer and language model
         self.tokenizer = AutoTokenizer.from_pretrained(
             context.artifacts["snapshot"], padding_side="left"
         )
-
-        # config = AutoConfig.from_pretrained(
-        #     context.artifacts["snapshot"], trust_remote_code=True
-        # )
-        # If you are running this in a system that has a sufficiently powerful GPU with available VRAM,
-        # uncomment the configuration setting below to leverage triton.
-        # Note that triton dramatically improves the inference speed performance
-
-        # config.attn_config["attn_impl"] = "triton"
 
         self.model = AutoModelForCausalLM.from_pretrained(
             context.artifacts["snapshot"],
@@ -102,61 +116,67 @@ class Deploy_model(mlflow.pyfunc.PythonModel):
         '''
 
         return {"predictions": [generated_response]}
+    
+
+def main(config_path):
+
+    with open(config_path, "r") as yaml_file:
+        config_data = yaml.safe_load(yaml_file)
+
+    snapshot_location = load_model(config_data)
+
+    ## Log the model using MLflow 
+    parameters = config_data['Deployment']['parameters']
+    input_example = pd.DataFrame({"prompt": config_data['Examples']['input_example']})
+    output_example = pd.DataFrame({"predictions": config_data['Examples']['output_example']})
+    signature = infer_signature(input_example, output_example, params=parameters)
+    
+    ### save the model in unity catalog
+    ### NOTE: this catalog should be given perssion for the peoplewho run the project 
+    mlflow.set_registry_uri(config_data['Deployment']['unity_catalog']['unity_catalog_uri'])
+    catalog =config_data['Deployment']['unity_catalog']['unity_catalog_name']
+    schema = config_data['Deployment']['unity_catalog']['unity_catalog_schema']
+    model_name = config_data['Model']['Name'].split('/')[-1]
+    registered_model_name = f"{catalog}.{schema}.{model_name}"
+
+    with mlflow.start_run(run_name="llm_as_pyfunc") as run:
+        ### log the model information 
+        mlflow.log_param(config_data['Model'])
+        model_info = mlflow.pyfunc.log_model(
+            artifact_path="llama_deployment_uc",
+            python_model=Deploy_model(),
+            ### NOTE: the artifacts dictionary mapping is critical! This dict is used by the load_context() method in our MPT() class.
+            artifacts={"snapshot": snapshot_location},
+            pip_requirements=config_data['pip_reqirements'],
+            input_example=input_example,
+            signature=signature,
+        )
+    mlflow.register_model(f'runs:/{run.info.run_id}/llama_deployment_uc', registered_model_name)
+
+    ## configure and create the model serving endpoint 
+    endpoint_name = model_name
+    model_name = registered_model_name
+    model_version = MlflowClient().get_registered_model(model_name).latest_versions[0].version 
+    ### choose the workload type according to quantization and parameter size 
+    quantization = config_data['Deployment']['quantization']
+    parameter_size = config_data['Deployment']['parameter_size']
+    workload_type = choose_workload_size(quantization, parameter_size)
+    workload_size = "Small" 
+    scale_to_zero = False 
+    API_ROOT = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().get() 
+    API_TOKEN = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
 
 
-# Get the current base version of torch that is installed, without specific version modifiers
-# torch_version = torch.__version__.split("+")[0]
 
-# Start an MLflow run context and log the MPT-7B model wrapper along with the param-included signature to
-# allow for overriding parameters at inference time
-'''
-parameters = {"temperature":0.1,"max_tokens":150}
-
-input_example = pd.DataFrame({"prompt": ["Hello, I'm a language model,"]})
-output_example = pd.DataFrame({"predictions": ["How can I help you"]})
-# inference_config={"max_new_tokens": 50, "temperature": 0.1}
-signature = infer_signature(input_example, output_example, params=parameters)
-
-# signature = ModelSignature(inputs=input_schema, outputs=output_schema, params=parameters)
-
-
-# Define input example
-input_example = pd.DataFrame({"prompt": ["What is machine learning?"]})
-
-mlflow.set_registry_uri('databricks-uc')
-CATALOG ="capgemini"
-SCHEMA = "chatbot"
-registered_model_name = f"{CATALOG}.{SCHEMA}.llama_pyfunc_model"
-
-with mlflow.start_run(run_name="llm_as_pyfunc") as run:
-    model_info = mlflow.pyfunc.log_model(
-        artifact_path="llama_deployment_uc",
-        python_model=Llama_pyfunc(),
-        # NOTE: the artifacts dictionary mapping is critical! This dict is used by the load_context() method in our MPT() class.
-        artifacts={"snapshot": snapshot_location},
-        pip_requirements=['pandas==1.4.4',
-    'torch==2.1.0',
-    'transformers==4.34.0',
-    'accelerate==0.23.0',
-    'bitsandbytes==0.41.1',
-    'tiktoken==0.5.1'],
-        input_example=input_example,
-        signature=signature,
-    )
-
-latest_model = mlflow.register_model(f'runs:/{run.info.run_id}/llama_deployment_uc', registered_model_name)
-'''
 
 if __name__ == "__main__":
 
-    import argparse
+    #import argparse
+    #parser = argparse.ArgumentParser(description="Description of your script")
+    #parser.add_argument("--config", type=str, help="Path to configration file")
+    #args = parser.parse_args()
 
-    # 
-    parser = argparse.ArgumentParser(description="Description of your script")
-    parser.add_argument("--config", type=str, help="Path to configration file")
-    args = parser.parse_args()
-
-
-    load_model(args.config)
+    config = "./configs/deployment_config.yaml"
+    main(config)
 
 
--- test.py
+++
@@ -1,5 +0,0 @@
-import yaml
-
-# Load the YAML config file
-with open("config.yaml", "r") as yaml_file:
-    config_data = yaml.safe_load(yaml_file)
